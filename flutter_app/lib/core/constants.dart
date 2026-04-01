e2a206a623c551276b7766c33a4c985cd77eb651
rakeshrachapudi <rakeshrachapudi04@gmail.com>
2026-04-01 14:56:43 +0530

    Port changed to live port in flutter constants

--- flutter_app/lib/core/constants.dart
+++ flutter_app/lib/core/constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String baseUrl = 'http://192.168.0.114:5000/api';
-// For physical Android phone on same Wi-Fi: http://YOUR_LAPTOP_IP:5000/api
-// For deployed AWS backend later: http://YOUR_EC2_PUBLIC_IP/api
+  static const String baseUrl = 'https://flodo-task-manager-backend.onrender.com/api';
+  // static const String baseUrl = 'http://192.168.0.114:5000/api';
+
 }
